da679c54e39c63b9540645ecc5c4375fa430030e
Suhas Suraj <suhas@suhass-mbp.dynamic.ucsd.edu>
2016-05-09 11:54:27 -0700

    post custom

--- predictor.c
+++ predictor.c
@@ -8,6 +8,7 @@
 
 #include "stdio.h"
 #include "predictor.h"
+#include "math.h"
 
 //
 // TODO:Student Information
@@ -55,6 +56,21 @@ int chooser;
 uint8_t *choose_cntr;
 void TOURNAMENT_update(uint32_t pc, uint8_t outcome);
 //end tournament
+
+//PWL
+#define ADDR_LENGTH 4
+#define GHIST_LENGTH 30
+#define SAT_VALUE 127
+int weights[(1<< ADDR_LENGTH)][(1<< ADDR_LENGTH)][GHIST_LENGTH+1];
+int ghistorylength;
+uint8_t gHistReg[GHIST_LENGTH];
+uint32_t addr[GHIST_LENGTH];
+int output;
+uint8_t prediction;
+int theta;
+void CUSTOM_update(uint32_t pc, uint8_t outcome);
+
+//end PWL
 //
 //TODO: Add your own Branch Predictor data structures here
 //
@@ -112,6 +128,34 @@ uint8_t tournament(uint32_t pc)
 	
 }
 
+uint8_t custom(uint32_t pc)
+{
+	pcMasked = (pc & mask);
+	output = weights[pcMasked][0][0];
+	int i;
+	for(i=1; i<= ghistorylength; i++)
+	{
+		if(gHistReg[i] == TAKEN)
+		{
+			output = output + weights[pcMasked][(addr[i])][i];
+		}
+		else
+		{
+			output = output - weights[pcMasked][(addr[i])][i];
+		}
+	}
+	if(output >= 0)
+	{
+		prediction = TAKEN;
+		return prediction;
+	}
+	else
+	{
+		prediction = NOTTAKEN;
+		return prediction;
+	}
+
+}
 //------------------------------------//
 //        Predictor Functions         //
 //------------------------------------//
@@ -194,6 +238,31 @@ init_predictor()
         }
         break;
         case CUSTOM:
+        {
+        	//TODO make all arrays 0
+        	mask = ((1 << ADDR_LENGTH) -1);
+        	ghistorylength = GHIST_LENGTH;
+        	theta = 2.14*(GHIST_LENGTH + 1) + 20.58;
+        	//theta=32;
+        	int i,j,k;
+        	for(i=0; i< (1<< ADDR_LENGTH); i++)
+        	{
+        		for(j=0; j < (1<< ADDR_LENGTH); j++)
+        		{
+        			for(k=0; k<= GHIST_LENGTH; k++)
+        			{
+        				weights[i][j][k]=0;
+        			}
+        		}
+        	}
+        	for(k=0; k< GHIST_LENGTH; k++)
+        	{
+        		gHistReg[k] = 0;
+				addr[k] = 0;
+			}
+        			       	
+        }
+        break;
         default:
             break;
     }
@@ -228,6 +297,8 @@ make_prediction(uint32_t pc)
         	return tournament(pc);
         	break;
         case CUSTOM:
+        	return custom(pc);
+        	break;
         default:
             break;
     }
@@ -260,6 +331,7 @@ train_predictor(uint32_t pc, uint8_t outcome)
         	TOURNAMENT_update(pc, outcome);
         	break;
         case CUSTOM:
+        	CUSTOM_update(pc, outcome);
         	break;
         default:
             break;
@@ -391,4 +463,47 @@ train_predictor(uint32_t pc, uint8_t outcome)
 	GHR = GHR & global_mask; 
 	//end global part
  
+ }
+ 
+ void CUSTOM_update(uint32_t pc, uint8_t outcome)
+ {
+ 	pcMasked = pc & mask;
+ 	if (abs(output) < theta || prediction != outcome)
+ 	{
+ 		if(outcome == TAKEN)
+ 		{
+ 			if(weights[pcMasked][0][0] < SAT_VALUE)
+ 				weights[pcMasked][0][0] = weights[pcMasked][0][0]+1;
+ 		}
+ 		else
+ 		{
+ 			if(weights[pcMasked][0][0] > ((-1)*SAT_VALUE))
+	 			weights[pcMasked][0][0] = weights[pcMasked][0][0]-1;
+
+ 		}
+ 		int i;
+ 		for (i=1; i <= ghistorylength; i++)
+ 		{
+ 			if(gHistReg[i] == outcome)
+			{
+				if( weights[pcMasked][(addr[i])][i] < SAT_VALUE)
+					weights[pcMasked][(addr[i])][i] = weights[pcMasked][(addr[i])][i] + 1;
+			}
+			else
+			{
+				if(weights[pcMasked][(addr[i])][i] > ((-1)*SAT_VALUE ))
+					weights[pcMasked][(addr[i])][i] = weights[pcMasked][(addr[i])][i] - 1;
+			}
+		}
+	}
+	int i;
+	for(i=ghistorylength-1; i>=2; i--)
+	{
+		addr[i]=addr[(i-1)];
+		gHistReg[i] = gHistReg[(i-1)];
+		
+	}
+	addr[1]=pcMasked;
+	gHistReg[1]=outcome;
+	
  }
